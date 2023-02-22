c0ac80e61f553240227e634585f100f6c977725c
Silvan Moura <silvancortesmoura@outlook.com>
2023-02-21 23:33:43 -0300

    Full view framework

--- resources/views/pet-view.blade.php
+++ resources/views/pet-view.blade.php
@@ -2,9 +2,40 @@
 
 @section('content')
 
-    <section class="banner" style="background-image: url({{URL::asset('images/banner_cat.jpg')}})">Todos os animais</section>
-    <h2> disponíveis para adoção</h2>
-    <div class="container-create">
+    
+    <div class="container-view">
+        <!-- <div class="view-left" style="background-image: url({{URL::asset('images/acara.jpg')}})">imagem</div> -->
+        <div class="view-left">
+            <img src="{{URL::asset('images/acara.jpg')}}" class="item--image" />
+        </div>
+        
+        <div class="view-right">
+            <h1>Informações sobre o animal</h1>
+
+            <div>
+                <label class="view-text">Raça:</label>
+                <input class="view-text" type="text" value="{{$pet->breed}}" readonly>
+            </div>
+
+            <div>
+                <label class="view-text">Cor(es):</label>  
+                <input class="view-text" type="text" value="{{$pet->color}}" readonly>
+            </div>
+
+            <div>
+                <label class="view-text">Sexo:</label>
+                <input class="view-text" type="text" value="{{$pet->gender}}" readonly>
+            </div>
+
+            <div>
+                <label class="view-text" class="view-text">Idade:</label>
+                <input class="view-text" type="text" value="{{$pet->age}}" readonly>
+            </div>
+        </div>
+        
+
+        
+        
     </div>
 
 @endsection
