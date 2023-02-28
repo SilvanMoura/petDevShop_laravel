eb72d916f0f6d732367bca40ad8f4ef27641ef27
Silvan Moura <silvancortesmoura@outlook.com>
2023-02-27 23:57:56 -0300

    Displaying data

--- resources/views/pet-update.blade.php
+++ resources/views/pet-update.blade.php
@@ -5,35 +5,65 @@
     
     <div class="container-view">
         <div class="view-left">
-            <img src="{{URL::asset('images/acara.jpg')}}" class="item--image" />
+            <img src={{URL::asset("images/$pet->picture")}} class="item--image" />
         </div>
         
         <div class="view-right">
-            <h1>Informações sobre o animal</h1>
+            <h1>Atualizando adoção</h1>
 
-            <div>
-                <label class="view-text">Raça:</label>
-                <input class="view-text" type="text" value="{{$pet->breed}}">
-            </div>
+            <form action="/createAdoption" method="POST" enctype="multipart/form-data">
+                @csrf
+                
+                <div>
+                    <label for="picturePet">Imagem do animal:</label>
+                    <input type="file" id="picturePet" name="picturePet">
+                </div>
 
-            <div>
-                <label class="view-text">Cor(es):</label>  
-                <input class="view-text" type="text" value="{{$pet->color}}">
-            </div>
+                <div>
+                    <label for="genderPet">Gênero:</label>
+                    <input type="text" id="genderPet" name="genderPet" placeholder="Gênero do animal" value="{{$pet->gender}}">
+                </div>
 
-            <div>
-                <label class="view-text">Sexo:</label>
-                <input class="view-text" type="text" value="{{$pet->gender}}">
-            </div>
+                <div>
+                    <label for="breedPet">Raça:</label>
+                    <input type="text" id="breedPet" name="breedPet" placeholder="Raça do animal" value="{{$pet->breed}}">
+                </div>
 
-            <div>
-                <label class="view-text" class="view-text">Idade:</label>
-                <input class="view-text" type="text" value="{{$pet->age}}">
-            </div>
+                <div>
+                    <label for="colorPet">Cor:</label>
+                    <input type="text" id="colorPet" name="colorPet" placeholder="Cor(es) do animal" value="{{$pet->color}}">
+                </div>
 
-            <div class="container-view">
-                <a href="/"><button class="btn-view-adoption">Voltar</button></a>
-            </div>
+                <div>
+                    <label for="kindPet">Espécie:</label>
+                    <select name="kindPet" id="kindPet">
+                        <option value="dogs" @if ($pet->kind === 'dogs') selected @endif >Cachorro</option>
+                        <option value="cats" @if ($pet->kind === 'cats') selected @endif >Gato</option>
+                        <option value="fishes" @if ($pet->kind === 'fishes') selected @endif >Peixe</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label for="agePet">Idade:</label>
+                    <input type="number" id="agePet" name="agePet" placeholder="Idade do animal" value={{$pet->age}}>
+
+                    <select name="ageTimePet" id="ageTimePet">
+                        <option value="Mes(es)" @if ($pet->age === 'Mes(es)') selected @endif >Mes(es)</option>
+                        <option value="Ano(s)" @if ($pet->age === 'Ano(s)') selected @endif >Ano(s)</option>
+                    </select>
+                </div>
+
+                <div class="buttons">
+                    <div>
+                        <a href="/"><button class="btn-view-adoption">Voltar</button></a>
+                    </div>
+                    <div>
+                        <button type="submit" class="btn-view-adoption">Salvar</button>
+                
+                    </div>
+                </div>
+            </form>
+
         </div>
         
         
